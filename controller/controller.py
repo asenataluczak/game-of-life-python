c18498611482df8ce5a3641d8b2447b51f7e885d
asenataluczak <asenata.luczak@gmail.com>
2023-01-06 11:34:51 +0100

    created controller class

---
+++ controller/controller.py
@@ -0,0 +1,49 @@
+import numpy
+
+
+class Controller:
+
+    def __init__(self, view, model):
+        self.view = view
+        self.model = model
+        self.updating = False
+        self.running = True
+        self.view.draw_cells(model.grid, numpy.ndindex(model.grid.shape))
+
+
+    def update_screen(self):
+        self.view.update_screen()
+        self.mouse = self.view.get_mouse_position()
+
+    def update_cells(self):
+        if self.updating:
+            self.model.update()
+            self.view.draw_cells(self.model.grid, numpy.ndindex(self.model.grid.shape))
+
+    def get_user_input(self):
+        for event in self.view.get_event():
+            self.__handle_user_quit(event)
+            self.__handle_user_click(event)
+
+    def __handle_user_quit(self, event):
+        if event.type == self.view.get_event_type('QUIT'):
+            self.__quit()
+            self.running = False
+
+    def __handle_user_click(self, event):
+        if event.type == self.view.get_event_type('MOUSEBUTTONDOWN'):
+            if self.view.buttonPlayPause.collide(self.mouse):
+                self.updating = not self.updating
+                self.view.buttonPlayPause.switch(self.view.get_surface(), self.updating)
+                self.view.buttonRefresh.disableEnable(self.view.get_surface(), self.updating)
+                self.view.buttonNext.disableEnable(self.view.get_surface(), self.updating)
+            if self.view.buttonRefresh.collide(self.mouse) and not self.updating:
+                self.model.set_initial_position()
+                self.view.draw_cells(self.model.grid, numpy.ndindex(self.model.grid.shape))
+            if self.view.buttonNext.collide(self.mouse) and not self.updating:
+                self.model.update()
+                self.view.draw_cells(self.model.grid, numpy.ndindex(self.model.grid.shape))
+
+
+    def __quit(self):
+        self.view.quit()
--- main.py
+++ main.py
@@ -1,41 +1,18 @@
-import numpy
 from view.view import View
 from model.model import Model
+from controller.controller import Controller
 
 cells_amount_x = 70
 cells_amount_y = 70
 
 view = View(cells_amount_x, cells_amount_y)
 model = Model(cells_amount_x, cells_amount_y)
+controller = Controller(view, model)
 
-view.draw_cells(model.grid, numpy.ndindex(model.grid.shape))
 
-updating = False
-running = True
-while running:
-    view.update_screen()
-    mouse = view.get_mouse_position()
-
-    for event in view.get_event():
-        if event.type == view.get_event_type('QUIT'):
-            running = False
-        if event.type == view.get_event_type('MOUSEBUTTONDOWN'):
-            if view.buttonPlayPause.collide(mouse):
-                updating = not updating
-                view.buttonPlayPause.switch(view.get_surface(), updating)
-                view.buttonRefresh.disableEnable(view.get_surface(), updating)
-                view.buttonNext.disableEnable(view.get_surface(), updating)
-            if view.buttonRefresh.collide(mouse) and not updating:
-                model.set_initial_position()
-                view.draw_cells(model.grid, numpy.ndindex(model.grid.shape))
-            if view.buttonNext.collide(mouse) and not updating:
-                model.update()
-                view.draw_cells(model.grid, numpy.ndindex(model.grid.shape))
-
-    if updating:
-        model.update()
-        view.draw_cells(model.grid, numpy.ndindex(model.grid.shape))
-        
-quit()
+while controller.running:
+    controller.update_cells()
+    controller.update_screen()
+    controller.get_user_input()   
 
 
